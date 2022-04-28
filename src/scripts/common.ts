20047d667cb14b3dae1ba2d00aa16e2eff3c88f0
Ozren Srzich <ozren.srzich@orchard.com.au>
2022-04-28 21:27:13 +1000

    feat (solar): added solar system w/ rotation

--- src/scripts/common.ts
+++ src/scripts/common.ts
@@ -1,77 +1,75 @@
 import { Canvas } from './modules/canvas';
 import { Sprite } from './modules/sprite';
 
+const PI2 = Math.PI * 2;
+
+const sun = new Sprite({
+  x: 100,
+  y: 100,
+  width: 400,
+  height: 400,
+  color: '#f66',
+  speed: 0,
+});
+
+const earth = new Sprite({
+  x: 420,
+  y: 0,
+  width: 40,
+  height: 40,
+  color: '#66c',
+  speed: 60000,
+});
+
+const moon = new Sprite({
+  x: 480,
+  y: 0,
+  width: 10,
+  height: 10,
+  color: '#ccc',
+  speed: 6000,
+});
+
+const getRotation = function (time: Date, speed: number, delimiter: number): number {
+  return ((2 * Math.PI) / delimiter) * time.getSeconds() + ((2 * Math.PI) / speed) * time.getMilliseconds();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const PI2 = Math.PI * 2;
   const canvas = new Canvas('.stage');
-  const sun = new Sprite({
-    x: 100,
-    y: 100,
-    width: 400,
-    height: 400,
-    color: '#f00',
-    speed: 1,
-  });
-
-  const earth = new Sprite({
-    x: 100,
-    y: 100,
-    width: 40,
-    height: 40,
-    color: '#f00',
-    speed: 1,
-  });
-
-  const moon = new Sprite({
-    x: 140,
-    y: 140,
-    width: 10,
-    height: 10,
-    color: '#f00',
-    speed: 1,
-  });
-
   canvas.context.globalCompositeOperation = 'destination-over';
-
-
 
   //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations
   canvas.keyframe((ctx) => {
-    // const time = new Date();
+    const time = new Date();
 
     canvas.clear();
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-    ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
     ctx.save();
-    // ctx.translate(canvas.centerX, canvas.centerY);
-
-    ctx.beginPath();
-    ctx.moveTo(canvas.centerX, canvas.centerY);
-    ctx.arc(canvas.centerX, canvas.centerY, sun.width / 2, 0, PI2);
-
-    ctx.moveTo(earth.x, earth.y);
-    ctx.arc(earth.x, earth.y, earth.width / 2, 0, PI2);
-
-    ctx.moveTo(moon.x, moon.y);
-    ctx.arc(moon.x, moon.y, moon.width / 2, 0, PI2);
-    ctx.stroke();
-    ctx.fill();
-
+    
     // Earth
-    // ctx.rotate(
-    //   ((2 * Math.PI) / 60) * time.getSeconds() +
-    //     ((2 * Math.PI) / 60000) * time.getMilliseconds()
-    // );
-    // // ctx.translate(105, 0);
-    // ctx.fillRect(earth.x, earth.y, earth.width, earth.height);
+    ctx.translate(canvas.centerX, canvas.centerY);
+    ctx.rotate(getRotation(time, earth.speed, 60));
+    ctx.translate(-canvas.centerX, -canvas.centerY);
+    ctx.beginPath();
+    ctx.arc(canvas.centerX + earth.x, canvas.centerY + earth.y, earth.width / 2, 0, PI2);
+    ctx.fillStyle = earth.color;
+    ctx.fill();
+    ctx.save();
+    
+    // Moon
+    ctx.translate(canvas.centerX + earth.x, canvas.centerY + earth.y);
+    ctx.rotate(getRotation(time, moon.speed, 6));
+    ctx.translate(-(canvas.centerX + earth.x), -(canvas.centerY + earth.y));
+    ctx.beginPath();
+    ctx.arc(canvas.centerX + moon.x, canvas.centerY + moon.y, moon.width / 2, 0, PI2);
+    ctx.fillStyle = moon.color;
+    ctx.fill();
+    ctx.restore();
 
-    // // Moon
-    // ctx.save();
-    // ctx.rotate(
-    //   ((2 * Math.PI) / 6) * time.getSeconds() +
-    //     ((2 * Math.PI) / 6000) * time.getMilliseconds()
-    // );
-    // ctx.translate(0, 28.5);
-    // ctx.restore();
+    // Sun
+    ctx.beginPath();
+    ctx.arc(canvas.centerX, canvas.centerY, sun.width / 2, 0, PI2);
+    ctx.fillStyle = sun.color;
+    ctx.fill();
+    ctx.restore();
   });
 });
